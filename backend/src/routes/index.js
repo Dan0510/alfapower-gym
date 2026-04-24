d521dd0e6eaf06cd4b3a9a9ebaa6b5893dc20c1d
Daniel personal <fd.aguilar.vargas@gmail.com>
2026-04-24 00:26:53 -0600

    Mail

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/auth', require('./auth/auth.routes'));
+router.use('/members', require('./members/members.routes'));
 
 module.exports = router;
