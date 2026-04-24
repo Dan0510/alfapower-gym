c5ffae3d5770abf67983a07c8875e2ee08948e7b
Daniel personal <fd.aguilar.vargas@gmail.com>
2026-04-24 07:16:31 -0600

    buckect

--- backend/src/config/gcp/gcpSecretManager.js
+++ backend/src/config/gcp/gcpSecretManager.js
@@ -2,13 +2,14 @@ const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
 const client = new SecretManagerServiceClient();
 
-async function getSecret(secretName) {
-  const [version] = await client.accessSecretVersion({
-    name: `projects/182411964865/secrets/${secretName}/versions/latest`
-  });
+async function getSecret(name, { parse = false } = {}) {
+    const [version] = await client.accessSecretVersion({
+        name: `projects/PROJECT_ID/secrets/${name}/versions/latest`,
+    });
 
-  const payload = version.payload.data.toString();
-  return JSON.parse(payload);
+    const value = version.payload.data.toString();
+
+    return parse ? JSON.parse(value) : value;
 }
 
 module.exports = { getSecret };
