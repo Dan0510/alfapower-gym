0e5e08530a0e07eab08ef3c1fe6a2545e0fa1182
Daniel personal <fd.aguilar.vargas@gmail.com>
2026-04-24 15:21:22 -0600

    nuevos servicios

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,6 +6,6 @@ router.use('/members', require('./members/members.routes'));
 router.use('/payments', require('./payments/payments.routes'));
 router.use('/memberships', require('./memberships/memberships.routes'));
 router.use('/payments-methods', require('./payments/payments-methods.routes'));
-router.use('/payment-folio', require('./payments/payment-folio.routes'));
+router.use('/payment-folio', require('./payments/payments-folio.routes'));
 
 module.exports = router;
