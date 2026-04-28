105ec33494109d1444cd8bf8c1c31ae448828c74
Daniel personal <fd.aguilar.vargas@gmail.com>
2026-04-28 17:13:08 -0600

    Se creó el servicio historial de pagos

--- backend/src/routes/payments/payments.routes.js
+++ backend/src/routes/payments/payments.routes.js
@@ -7,5 +7,5 @@ router.post('/create', PaymentsController.createPayment);
 router.get('/today/:id_gym_branch', PaymentsController.getTodayPayments);
 router.post('/filter', PaymentsController.filterPayments);
 router.post('/cancel/:id_payment', PaymentsController.cancelPayment);
-
+  
 module.exports = router;
