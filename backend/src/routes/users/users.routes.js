e99bd82aa65b2dd3c28dbd874560c88380dfd827
Daniel personal <fd.aguilar.vargas@gmail.com>
2026-04-30 15:31:05 -0600

    Se creó el servicio de usuarios

--- backend/src/routes/users/users.routes.js
+++ backend/src/routes/users/users.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const UsersController = require('../../controllers/users/users.controller');
 
-router.post('/', UsersController.create);
+router.post('/create', UsersController.create);
 router.get('/:id_gym_branch', UsersController.getAll);
 router.put('/:id_user', UsersController.update);
 router.put('/password/:id_user', UsersController.updatePassword);
