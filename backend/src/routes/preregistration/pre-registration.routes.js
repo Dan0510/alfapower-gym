38526700a53c168256def45211ae0a0616adef41
Daniel personal <fd.aguilar.vargas@gmail.com>
2026-05-15 16:58:17 -0600

    Se creó el servicio para guardar y obtener los comentarios para la página web

--- backend/src/routes/preregistration/pre-registration.routes.js
+++ backend/src/routes/preregistration/pre-registration.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const PreRegistrationController = require('../../controllers/pre-registration/pre-registration.controller');
+const PreRegistrationController = require('../../controllers/preregistration/pre-registration.controller');
 
 router.post('/create', PreRegistrationController.createPreRegistration);
 
