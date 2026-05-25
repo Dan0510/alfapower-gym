6d33c43a6edd3b76dc694cdedbefc8fa0e174d80
Daniel personal <fd.aguilar.vargas@gmail.com>
2026-05-25 12:33:08 -0600

    Se quitó el envió por email

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,8 @@ const corsOptions = {
       'http://localhost:8080',
       'https://alfapowergym.com',
       'https://admin.alfapowergym.com',
-      'https://service-alfapower-gym-frontend-182411964865.us-central1.run.app'
+      'https://service-alfapower-gym-frontend-182411964865.us-central1.run.app',
+      'https://service-athletic-gym-frontend-182411964865.us-central1.run.app'
     ];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
